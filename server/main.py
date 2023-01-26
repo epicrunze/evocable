e3f52afaa7d59a223af9d08835f1a99578257cbb
epicrunze <epicrunze@gmail.com>
2023-01-25 20:57:48 -0500

    backend test

--- server/main.py
+++ server/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 
 '''
-
+Main code for the fastapi backend to predict
 '''
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.post("/simple_text/{query}")
+async def simple_text(query: str):
+    '''
+    Args:
+        query (str): an in-url string for the sentence to be predicted
+    '''
+
     return {"message": "Hello World"}
